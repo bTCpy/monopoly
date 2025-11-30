1717d8b2894e0a6d1c756e0536ef1407d594f599
bTCpy <133805753+bTCpy@users.noreply.github.com>
2025-11-30 05:08:18 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,8 +7,32 @@ const urlsToCache = [
   '/classicedition.js',
   '/ai.js',
   '/images/chance_icon.png',
-  '/images/community_chest_icon.png'
-  // Add other images here if you want them to load offline
+  '/images/community_chest_icon.png',
+  '/images/arrow.png',
+  '/images/arrow_icon.png',
+  '/images/chance_icon.png',
+  '/images/close.png',
+  '/images/community_chest_icon.png',
+  '/images/Die_1.png',
+  '/images/Die_2.png',
+  '/images/Die_3.png',
+  '/images/Die_4.png',
+  '/images/Die_5.png',
+  '/images/Die_6.png',
+  '/images/electric_icon.png',
+  '/images/free_parking_icon.png',
+  '/images/hori-bar.png',
+  '/images/hotel.png',
+  '/images/hotel_faded.png',
+  '/images/house.png',
+  '/images/house_faded.png',
+  '/images/jake_icon.png',
+  '/images/menu_background.png',
+  '/images/menu_background_hover.png',
+  '/images/tax_icon.png',
+  '/images/train_icon.png',
+  '/images/vert-bar.png',
+  '/images/water_icon.png'
 ];
 
 self.addEventListener('install', function(event) {
